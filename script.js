9e7b10e212c3fa274dd39eb81e69bd08c75781bc
rahuldhanotia3 <72001953+rahuldhanotia3@users.noreply.github.com>
2021-07-26 22:25:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Android Developer", "System Software Developer", "Freelancer"],
+        strings: ["Web Developer", "Android Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Android Developer", "System Software Developer", "Freelancer"],
+        strings: ["Web Developer", "Android Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
